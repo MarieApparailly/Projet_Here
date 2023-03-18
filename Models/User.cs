5d65231653a1f3dc3e461cc68d06e82c4f5484d4
MarieApparailly <mapparailly@ensc.fr>
2023-03-18 14:28:22 +0100

    Modif User (add aatributs + Team)

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,7 @@
 namespace Projet_Here.Models;
 
+
+// enum définissant le statut du membre
 public enum Status
 {
     EN_MISSION,
@@ -7,12 +9,16 @@ public enum Status
     EN_REPOS
 }
 
+
 public class User 
 {
     public int Id { get; set; }
 
-    public int PlaceId { get; set; }
-    public Place Place { get; set; } = null!;
+    public string Latitude { get; set; } 
+    public string Longitude { get; set; } 
+
+    public int? PlaceId { get; set; } // à associer ou pour remplacer Latitude et Longitude (en fonction de l'API de localisation)
+    public Place? Place { get; set; } 
 
     public string Pseudo { get; set; } = null!;
     public string Password { get; set; } = null!;
@@ -20,17 +26,37 @@ public class User
     public string LastName { get; set; } = null!;
     public string Phone { get; set; } = null!;
     public string? Mail { get; set; }
-    public Status Status { get; set; } 
+    public string? Photo { get; set; }
     public bool Car { get; set; }
-    public string Team { get; set; } = null!;
+
+    public Status Status { get; set; } 
+    public string Group { get; set; } = null!;
+    public List<Team>? Team { get; set; } // à utiliser à la place de Group selon avancement
+    public bool Admin { get; set; }
     public List<Mission> Missions { get; set; } = new List<Mission>();
 
+    // Constructeurs
     public User() { 
         Status = Status.DISPONIBLE;
+        Admin = true;
     }
 
-    public User(string team) {
-        Team = team;
+    public User(string group) {
+        Group = group;
+        Status = Status.DISPONIBLE;
+        Admin = true;
+    }
+
+    public User(string pseudo, string password, string firstName, string lastName, string phone, bool car, string group) {
+        Pseudo = pseudo;
+        Password = password;
+        FirstName = firstName;
+        LastName = lastName;
+        Phone = phone;
+        Car = car;
+        Group = group;
+        Status = Status.DISPONIBLE;
+        Admin = true;
     }
 
 }
