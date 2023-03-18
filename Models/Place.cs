cf246ca2198ee9e5739318edbafd11a3a82d0bce
MarieApparailly <mapparailly@ensc.fr>
2023-03-18 17:25:37 +0100

    Modif Place.cs

--- Models/Place.cs
+++ Models/Place.cs
@@ -5,6 +5,7 @@ public class Place
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public string City { get; set; } = null!;
+    public string? Description { get; set; } 
     public string? Latitude { get; set; }
     public string? Longitude { get; set; }
     public List<Mission> Missions { get; set; } = new List<Mission>();
